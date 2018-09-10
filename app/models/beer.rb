2b33b9f08f42db32b47bb181faa3f40e237ab017
Topi T <smobey@gmail.com>
2018-09-10 13:25:19 +0300

    t5

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,11 +3,8 @@ class Beer < ApplicationRecord
   has_many :ratings
 
   def average_rating
-    total = 0
-    ratings.each do |rating|
-      total += rating.score
-    end
-    average = total / ratings.count
+    scores = ratings.map{|rating| rating.score}
+    average = scores.reduce(:+).to_f / scores.size
     "Has #{ratings.count} ratings, average #{average}"
   end
 end
