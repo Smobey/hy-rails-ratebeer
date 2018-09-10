14e4baf07c0e7b705c17b936ceac04bb92802bf2
Topi T <smobey@gmail.com>
2018-09-10 13:10:57 +0300

    t4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,13 @@
-class Beer < ActiveRecord::Base
+class Beer < ApplicationRecord
   belongs_to :brewery
+  has_many :ratings
+
+  def average_rating
+    total = 0
+    ratings.each do |rating|
+      total += rating.score
+    end
+    average = total / ratings.count
+    "Has #{ratings.count} ratings, average #{average}"
+  end
 end
