c744de3efbb00161c1939092ac7616e57d851c5d
Topi T <smobey@gmail.com>
2018-09-10 14:54:33 +0300

    t11

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,5 @@
+include ActionView::Helpers::TextHelper
+
 class Beer < ApplicationRecord
   belongs_to :brewery
   has_many :ratings
@@ -5,6 +7,10 @@ class Beer < ApplicationRecord
   def average_rating
     scores = ratings.map{|rating| rating.score}
     average = scores.reduce(:+).to_f / scores.size
-    "Has #{ratings.count} ratings, average #{average}"
+    "Has #{pluralize(ratings.count, 'rating')}, average #{average}"
+  end
+
+  def to_s
+    "#{self.brewery.name}: #{self.name}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :beers
+
   resources :breweries
-  get 'ratings', to: 'ratings#index'
+
+  resources :ratings, only: [:index, :new, :create, :destroy]
+
   root 'breweries#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
