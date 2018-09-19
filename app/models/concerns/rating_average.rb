0f32e847c9a02c211c78b4539aae2252b5910171
Topi T <smobey@gmail.com>
2018-09-19 18:56:58 +0300

    t3

--- app/models/concerns/rating_average.rb
+++ app/models/concerns/rating_average.rb
@@ -2,7 +2,7 @@ module RatingAverage
   extend ActiveSupport::Concern
 
   def average_rating
-    scores = ratings.map(&:rating)
+    scores = ratings.map(&:score)
     average = scores.reduce(:+).to_f / scores.size
     "Has #{pluralize(ratings.count, 'rating')}, average #{average}"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :users
   resources :beers
-
   resources :breweries
-
   resources :ratings, only: [:index, :new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
+
+  get 'signup', to: 'users#new'
 
   root 'breweries#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
